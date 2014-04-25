b1361b3c4e93ae8ef3d3a37785005bee7b2288ee
Nicholas Curtis <nich.curtis@gmail.com>
2014-04-25 01:33:05 -0700

    kaggle digits data set
    http://www.kaggle.com/c/digit-recognizer

---
+++ datasets/digits.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var csv = require('csv');
+var Q = require('q');
+
+module.exports = function () {
+
+    var dataSet = {
+        train: {
+            features: [],
+            targets: []
+        },
+        validation: {
+            features: [],
+            targets: []
+        },
+        test: {
+            features: []
+        }
+    };
+
+    var dSet = 'train';
+    var deferred = Q.defer();
+ 
+    csv()
+        .from.path(__dirname+'/digits-train.csv', { delimiter: ',', escape: '"' })
+        .on('record', function (row,index){
+            if (index > 38000) dSet = 'validation';
+            dataSet[dSet].features.push(row.slice(1));
+            dataSet[dSet].targets.push(row[0]);
+        })
+        .on('end', function (count) {
+            deferred.resolve(dataSet); 
+        })
+        .on('error', function (error) {
+            deferred.reject(error);
+        });
+ 
+    return deferred.promise;
+};
