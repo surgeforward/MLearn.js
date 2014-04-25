fe0944a486236b08bc57f8becb7acb4c14dedba9
Nicholas Curtis <nich.curtis@gmail.com>
2014-04-25 08:37:42 -0700

    updated to use promises and added benchmarks

--- example/knn/classifier.js
+++ example/knn/classifier.js
@@ -1,20 +1,44 @@
 var getDataSet = require(__dirname+'/../../datasets/digits.js');
 
+var startTime = Date.now();
+
 console.log('Loading Digits Dataset');
 getDataSet().then(function (dataSet, getTestData) {
-	console.log('Digits Dataset Loaded');
+    
+    console.log('Digits Dataset Loaded');
+    var mlearn = require(__dirname+'/../../index.js')();
+    var knn = mlearn.classifier('knn', { neighbors: 5 });
+    console.log('Training Model');
 
-	var mlearn = require(__dirname+'/../../index.js')();
-	var knn = mlearn.classifier('knn', { neighbors: 5 });
+    knn.train(dataSet.train.features, dataSet.train.targets)
+        .then(function () {
+        
+            console.log('Model Trained');
+            console.log('Scoring Model With Validation Data');
+            var scoreStart = Date.now();
 
-	console.log('Training Model');
-	knn.train(dataSet.train.features, dataSet.train.targets);
-	console.log('Model Trained');
+            knn.score(dataSet.validation.features, dataSet.validation.targets)
+                .then(function (score) {
+                    
+                    console.log('Finished Scoring');
+                    console.log('Accuracy: ', (score*100)+'%');
+                    console.log('Completed Scoring In', (Date.now()-scoreStart)/1000, 'Seconds');
+                    console.log('Completed All In', (Date.now()-startTime)/1000, 'Seconds');
 
-	console.log('Scoring Model With Validation data');
-	var score = knn.score(dataSet.validation.features, dataSet.validation.targets);
-	console.log('Finished Scoring: ', score);
+                }, function (error) {
+
+                    console.log(error);
+
+                });
+        
+        }, function (error) {
+            
+            console.log(error);
+        
+        });
 
 }, function (error) {
+    
     console.log(error);
+
 });
