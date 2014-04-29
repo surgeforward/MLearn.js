781083390877a958e6e5c921fa9e83a8f285af48
Nicholas Curtis <nich.curtis@gmail.com>
2014-04-29 06:46:56 -0700

    optimziation for single threaded predictions

--- datasets/digits.js
+++ datasets/digits.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var fs = require('fs');
 var csv = require('csv');
 var Q = require('q');
@@ -22,13 +23,20 @@ module.exports = function () {
     var deferred = Q.defer();
  
     csv()
-        .from.path(__dirname+'/digits-train.csv', { delimiter: ',', escape: '"' })
+        .from.path(__dirname+'/digits/train.csv', { delimiter: ',', escape: '"' })
         .on('record', function (row,index){
-            if (index > 41990) dSet = 'validation';
-            dataSet[dSet].features.push(row.slice(1));
+            dSet = (index < 38000) ? 'train' : 'validation' ;
+            if (index >= 38001) return;
+            
+            var features = _.map(row.slice(1), function (feature) {
+                return feature > 0 ? 1 : 0 ;
+            });
+            dataSet[dSet].features.push(features);
             dataSet[dSet].targets.push(row[0]);
         })
         .on('end', function (count) {
+            console.log('Traing Data Size: ' + dataSet.train.features.length + ' records and ' + dataSet.train.features[0].length + ' features');
+            console.log('Validation Data Size: ' + dataSet.validation.features.length + ' records and ' + dataSet.validation.features[0].length + ' features');
             deferred.resolve(dataSet); 
         })
         .on('error', function (error) {
