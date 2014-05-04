5cbf3751474d99df4a7e8dff86d6682eb04b60d2
Nicholas Curtis <ncurtis@surgeforward.com>
2014-05-03 20:20:59 -0700

    updated lr classifier example to match api

--- example/logistic/classifier.js
+++ example/logistic/classifier.js
@@ -1,29 +1,28 @@
 var util = require('util');
 var Q = require('q');
 
-var getDataSet = require(__dirname + '/../../datasets/digits.js');
-
 var startTime = Date.now(), dataStart = Date.now();
 var mlearn = require(__dirname + '/../../mlearn.js')();
-var knn, scoreStart, trainStart;
+var logistic, scoreStart, trainStart;
 
 Q.longStackSupport = true;
 
 console.log('Loading Dataset...');
-getDataSet().then(function (dataSet, getTestData) {
+mlearn.data.from.csv(__dirname+'/datasets/digits/train.csv')
+.then(function (dataSet, getTestData) {
     console.log('Finished Loading Dataset in', (Date.now() - dataStart) / 1000, 'Seconds');
-    knn = mlearn.classifier('logistic');
+    logistic = mlearn.classifier('logistic', {iterations: 100, alpha: 0.001, lambda: 0.001});
     console.log('Training Model...');
 
     trainStart = Date.now();
-    return knn.training(dataSet.train.features, dataSet.train.targets).then(function () {
+    return logistic.training(dataSet.train.features, dataSet.train.targets).then(function () {
         console.log('Completed Training in', (Date.now() - trainStart) / 1000, 'Seconds');
         return dataSet;
     });
 }).then(function (dataSet) {
     scoreStart = Date.now();
     console.log('Scoring Model...');
-    return knn.scoring(dataSet.validation.features, dataSet.validation.targets);
+    return logistic.scoring(dataSet.validation.features, dataSet.validation.targets);
 }).then(function (score) {
     console.log('Finished Scoring W/ Accuracy of', (score * 100) + '%');
     console.log('Completed Scoring in', (Date.now() - scoreStart) / 1000, 'Seconds');
