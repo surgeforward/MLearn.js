269ea86c97bcf069bb10b6f402bd62819656bc6f
Nicholas Curtis <ncurtis@surgeforward.com>
2014-05-11 15:46:58 -0700

    change logging to util.log, new cdv path var

--- example/knn/classifier.js
+++ example/knn/classifier.js
@@ -11,21 +11,24 @@ var knn, scoreStart, trainStart;
 Q.longStackSupport = true;
 
 var numNeighbors = parseInt(process.argv[2]) || 5 ;
-var trainSize = process.argv[3].split(',') || [27000,3000] ;
+var trainSize = process.argv[3] || '27000,3000' ;
+trainSize = trainSize.split(',');
 var metricType = process.argv[4] || 'euclidian' ;
 var weightedKNN = process.argv[5] || false ;
 
-console.log('Loading Dataset...');
-getDataSet(parseInt(trainSize[0]), parseInt(trainSize[1]))
+var pathToCSV = 'path/to/mlearn/kaggle-hwdigit/train.csv';
+
+util.log('Loading Dataset...');
+getDataSet(pathToCSV, parseInt(trainSize[0]), parseInt(trainSize[1]))
     .then(function (dataSet, getTestData) {
 
         knn = mlearn.classifier('knn', { neighbors: parseInt(numNeighbors), metric: metricType, weights: (weightedKNN) ? true : false });
-        console.log('Training Model...');
+        util.log('Training Model...');
 
         trainStart = Date.now();
         return knn.training(dataSet.train).then(function () {
             
-            console.log('Completed Training in', (Date.now() - trainStart) / 1000, 'Seconds');
+            util.log('Completed Training in ' + ((Date.now() - trainStart) / 1000) + ' seconds');
             return dataSet;
 
         });
@@ -33,20 +36,20 @@ getDataSet(parseInt(trainSize[0]), parseInt(trainSize[1]))
     }).then(function (dataSet) {
 
         scoreStart = Date.now();
-        console.log('Scoring Model...');
+        util.log('Scoring Model...');
         return knn.scoring(dataSet.validation);
 
     }).then(function (score) {
        
-        console.log('Completed Scoring in', (Date.now() - scoreStart) / 1000, 'Seconds');
-        console.log('Completed All in', (Date.now() - startTime) / 1000, 'Seconds');
+        util.log('Completed Scoring in ' + ((Date.now() - scoreStart) / 1000) + ' seconds');
+        util.log('Completed All in ' + ((Date.now() - startTime) / 1000) + ' seconds');
 
-        console.log('Accuracy: ', score.accuracy());
-        console.log('Error: ', score.error());
+        util.log('Accuracy: ' + score.accuracy());
+        util.log('Error: ' + score.error());
 
-        console.log('Misses:');
+        util.log('Misses:');
         _.each(score.misses(), function (x) {
-            console.log('Predicted: ' + x.prediction + ', Target: ' + x.target);
+            util.log('Predicted: ' + x.prediction + ' Target: ' + x.target);
         });
 
     }).catch(function (error) {
