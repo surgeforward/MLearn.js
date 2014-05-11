9ac062fca4f265a4f2ad1cbd8292ded0c114fc67
Nicholas Curtis <ncurtis@surgeforward.com>
2014-05-11 15:49:18 -0700

    updated example with regressor/knn changes

--- example/knn/regressor.js
+++ example/knn/regressor.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var Q = require('q');
+var _ = require('lodash');
 
 var getDataSet = require(__dirname + '/../../datasets/houses.js');
 
@@ -9,27 +10,46 @@ var knn, scoreStart, trainStart;
 
 Q.longStackSupport = true;
 
-console.log('Loading Dataset...');
-getDataSet().then(function (dataSet, getTestData) {
-    console.log('Finished Loading Dataset in', (Date.now() - dataStart) / 1000, 'Seconds');
-    knn = mlearn.regressor('knn', { neighbors: 5 });
-    console.log('Training Model...');
+var numNeighbors = parseInt(process.argv[2]) || 5 ;
+var trainSize = process.argv[3] || '27000,3000' ;
+trainSize = trainSize.split(',');
+var metricType = process.argv[4] || 'euclidian' ;
 
-    trainStart = Date.now();
-    return knn.training(dataSet.train.features, dataSet.train.targets).then(function () {
-        console.log('Completed Training in', (Date.now() - trainStart) / 1000, 'Seconds');
-        return dataSet;
+var pathToCSV = '/Volumes/Crucial 240GB/Workspace/Surge/GitHub/mlearn-datasets/sample-houses/train.csv';
+//var pathToCSV = 'path/to/mlearn/sample-hours/train.csv';
+
+util.log('Loading Dataset...');
+getDataSet(pathToCSV, parseInt(trainSize[0]), parseInt(trainSize[1]))
+    .then(function (dataSet, getTestData) {
+
+        knn = mlearn.regressor('knn', { neighbors: parseInt(numNeighbors), metric: metricType});
+        util.log('Training Model...');
+
+        trainStart = Date.now();
+        return knn.training(dataSet.train).then(function () {
+            
+            util.log('Completed Training in ' + ((Date.now() - trainStart) / 1000) + ' seconds');
+            return dataSet;
+
+        });
+
+    }).then(function (dataSet) {
+
+        scoreStart = Date.now();
+        util.log('Scoring Model...');
+        return knn.scoring(dataSet.validation);
+
+    }).then(function (score) {
+       
+        util.log('Completed Scoring in ' + ((Date.now() - scoreStart) / 1000) + ' seconds');
+        util.log('Completed All in ' + ((Date.now() - startTime) / 1000) + ' seconds');
+
+        util.log('Error: ' + score.error());
+
+    }).catch(function (error) {
+
+        // catch any errors thrown by any of the above promises
+        util.error('Error at regressor.js promise chain:');
+        util.error(error.stack);
+
     });
-}).then(function (dataSet) {
-    scoreStart = Date.now();
-    console.log('Scoring Model...');
-    return knn.scoring(dataSet.validation.features, dataSet.validation.targets);
-}).then(function (score) {
-    console.log('Finished Scoring W/ Error of', score );
-    console.log('Completed Scoring in', (Date.now() - scoreStart) / 1000, 'Seconds');
-    console.log('Completed All in', (Date.now() - startTime) / 1000, 'Seconds');
-}).catch(function (error) {
-    // catch any errors thrown by any of the above promises
-    util.error('Error at regressor.js promise chain:');
-    util.error(error.stack);
-});
